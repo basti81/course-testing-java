21d73ea6ee76a39a5997043ca38dc116c6cd6fd8
basti81 <bastian.g.astudillo.r@gmail.com>
2023-03-01 23:30:30 -0600

    Commit 2: api rest employee con metodo save por implementar

--- src/main/java/com/javaguides/springboot/Employee.java
+++ src/main/java/com/javaguides/springboot/Employee.java
@@ -1,9 +1,15 @@
 package com.javaguides.springboot;
 
+import lombok.*;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 import javax.persistence.*;
 
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 @Table(name = "employee")
 public class Employee {
@@ -14,49 +20,5 @@ public class Employee {
     private String email;
     private String password;
 
-    public Employee() {
-    }
 
-    public Employee(String name, String email, String password) {
-        this(null,name,email,password);
-    }
-
-    public Employee(Long id, String name, String email, String password) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.password = password;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
--- src/test/java/com/javaguides/springboot/EmployeeControllerTest.java
+++ src/test/java/com/javaguides/springboot/EmployeeControllerTest.java
@@ -1,8 +1,63 @@
 package com.javaguides.springboot;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
+@WebMvcTest
 class EmployeeControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private EmployeeService employeeService;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
+    @Test
+    public void createObjectEmployee_returnSavedEmployee() throws Exception {
+
+        // Given - Peticion o configuracion
+        Employee employee = Employee.builder()
+                .name("Ramesh")
+                .email("ramesh@gmail.com")
+                .password("1234")
+                .build();
+        given(employeeService.saveEmployee(any(Employee.class)))
+                .willAnswer((invocation)-> invocation.getArgument(0));
+        given(employeeService.saveEmployee(any(Employee.class))).willAnswer((invocation)-> invocation.getMethod());
+        System.out.println("Clase creada -> " + employee.toString());
+
+        // When - Comportamiento que vamos a probar
+        ResultActions resultActions = mockMvc.perform(post("/api/employee")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(employee)));
+
+        // Then - Verificar el resultado de la peticion
+        resultActions.andDo(print()).
+                andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name",is(employee.getName())))
+                .andExpect(jsonPath("$.email", is(employee.getEmail())))
+                .andExpect(jsonPath("$.password", is(employee.getPassword())));
+
+
+    }
+
 
 }
